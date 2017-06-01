fba4ba165ddc9f3971da23fab9f51ed5deacc182
sureshdvp <suresh.devjava@gmail.com>
2017-06-01 17:22:20 +0530

    initial commits

---
+++ TEST/src/Test.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap map = new HashMap();
+		map.put("key1", "value1");
+		map.put("key1", "value1");
+		map.put("key1", "value1");
+
+	}
+
+}
